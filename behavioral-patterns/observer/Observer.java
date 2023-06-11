103bb9d54ac366e88a9b58f8981637bda5918db7
JeongTaek Oh <tei1234tei@gmail.com>
2023-06-11 15:30:47 +0900

    implement Observer pattern

---
+++ behavioral-patterns/observer/Observer.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Observer {
+    public static void main(String[] args) {
+        MyPrompt prompt = new MyPrompt();
+
+        prompt.registerObserver(new MyTestObserver());
+        prompt.registerObserver(new CommandCountObserver());
+        prompt.registerObserver(new CommandAnalyzeObserver());
+
+        prompt.run();
+    }
+}
+
+interface ObserverI<T> {
+    public void update(T data);
+}
+
+class BaseSubject<T> {
+    protected T state;
+
+    protected ArrayList<ObserverI<T>> observers;
+
+    public BaseSubject() {
+        observers = new ArrayList<>();
+    }
+
+    public void registerObserver(ObserverI<T> observer) {
+        observers.add(observer);
+    }
+
+    protected void notifyObservers() {
+        for (ObserverI<T> observer: observers) {
+            observer.update(state);
+        }
+    }
+}
+
+class MyPrompt extends BaseSubject<String> {
+    public MyPrompt() {
+        super();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Shell Running...");
+        System.out.println("Write your command:");
+
+        System.out.print(">> ");
+        while (scanner.hasNextLine()) {
+            state = scanner.nextLine();
+
+            notifyObservers();
+
+            System.out.print(">> ");
+        }
+    }
+}
+
+class MyTestObserver implements ObserverI<String> {
+    public void update(String data) {
+        System.out.println("MyTestObserver: " + data);
+    }
+}
+
+class CommandCountObserver implements ObserverI<String> {
+    private int count;
+
+    public CommandCountObserver() {
+        count = 0;
+    }
+
+    public void update(String data) {
+        count++;
+        System.out.println("CommandCountObserver: " + count);
+    }
+}
+
+class CommandAnalyzeObserver implements ObserverI<String> {
+    private HashMap<String, Integer> wordCount;
+
+    public CommandAnalyzeObserver() {
+        wordCount = new HashMap<>();
+    }
+
+    public void update(String data) {
+        String[] words = data.split(" ");
+
+        Arrays.stream(words)
+                .map(word -> word
+                        .toLowerCase()
+                        .replaceAll("[^a-zA-Z0-9]", "")
+                )
+                .forEach(word -> {
+                    if (wordCount.containsKey(word)) {
+                        wordCount.put(word, wordCount.get(word) + 1);
+                    } else {
+                        wordCount.put(word, 1);
+                    }
+                });
+
+//        for (String word: words) {
+//            if (wordCount.containsKey(word)) {
+//                wordCount.put(word, wordCount.get(word) + 1);
+//            } else {
+//                wordCount.put(word, 1);
+//            }
+//        }
+
+        System.out.println("CommandAnalyzeObserver: " + wordCount);
+    }
+}
